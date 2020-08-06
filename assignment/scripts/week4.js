b19026c9e1d9c69be063b66ac19c1e55bae663af
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-08-06 13:40:19 -0500

    completed function parkCar and function getInGarage

--- assignment/scripts/week4.js
+++ assignment/scripts/week4.js
@@ -1,1 +1,27 @@
-console.log('js');
+console.log( 'js' );
+
+let parkedCars = [];
+const maxCars = 5;
+
+function parkCar(){
+  console.log( 'in parkCar' );
+  if( maxCars >= parkedCars.length ){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+function getInGarage( nameOfCar ){
+  console.log( 'in getInGarage', nameOfCar );
+  parkedCars.push( nameOfCar );
+  return parkedCars;
+}
+
+getInGarage( 'Ford Explorer' );
+getInGarage( 'Nisson GTR' );
+getInGarage( 'Chevy Cruise' );
+getInGarage( 'Audi RS 5 Sportback' );
+
+console.log( parkCar() );
