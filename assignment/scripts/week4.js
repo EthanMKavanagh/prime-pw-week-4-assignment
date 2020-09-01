62f15653013d26c8d28170ff82e1577ccb0f8df6
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-08-31 19:55:05 -0500

    completing past assignments

--- assignment/scripts/week4.js
+++ assignment/scripts/week4.js
@@ -1,32 +1,32 @@
 console.log( 'js' );
 
 
-// parkCar Funtion
+// parkCar Function
 //----------------
 
 let parkedCars = [];
 const maxCars = 5;
 
 
-function parkCar( nameOfCar ){
+function parkCar( color, nameOfCar ){
   console.log( 'in parkCar' );
   if( parkedCars.length < maxCars ){
     parkedCars.push( nameOfCar );
-    console.log( 'Can I park the', nameOfCar, 'in the garage?' );
+    console.log( 'Can I park the', color, nameOfCar, 'in the garage?' );
     return true;
   } // end if
   else{
-    console.log( 'Can I park the', nameOfCar, 'in the garage?' );
+    console.log( 'Can I park the', color, nameOfCar, 'in the garage?' );
     return false;
   } // end else
 } // end parkCar
 
-console.log( parkCar( 'Ford Explorer' ) );
-console.log( parkCar( 'Nisson GTR' ) );
-console.log( parkCar( 'Chevy Cruise' ) );
-console.log( parkCar( 'Audi RS 5 Sportback' ) );
-console.log( parkCar( 'Toyota Highlander' ) );
-console.log( parkCar( 'Jeep Wrangler' ) );
+console.log( parkCar( 'black', 'Ford Explorer' ) );
+console.log( parkCar( 'blue', 'Nisson GTR' ) );
+console.log( parkCar( 'orange', 'Chevy Cruise' ) );
+console.log( parkCar( 'green', 'Audi RS 5 Sportback' ) );
+console.log( parkCar( 'red', 'Toyota Highlander' ) );
+console.log( parkCar( 'purple', 'Jeep Wrangler' ) );
 
 
 
@@ -36,25 +36,25 @@ console.log( parkCar( 'Jeep Wrangler' ) );
 //----------------------
 
 
-function isParkedHere( nameOfCar ){
+function isParkedHere( color, nameOfCar ){
   console.log( 'in isParkedHere' );
   for( let i=0; i<maxCars; i++ ){
     parkedCars.push( nameOfCar );
     if( nameOfCar === parkedCars[ i ] ){
-      console.log( 'Is the', nameOfCar , 'parked in the garage?' );
+      console.log( 'Is the', color, nameOfCar, 'parked in the garage?' );
       return true;
     } // end if
   } // end for
-  console.log( 'Is the', nameOfCar , 'parked in the garage?' );
+  console.log( 'Is the', color, nameOfCar, 'parked in the garage?' );
   return false;
 } // end isParkedHere
 
-console.log( isParkedHere( 'Ford Explorer' ) );
-console.log( isParkedHere( 'Nisson GTR' ) );
-console.log( isParkedHere( 'Chevy Cruise' ) );
-console.log( isParkedHere( 'Audi RS 5 Sportback' ) );
-console.log( isParkedHere( 'Toyota Highlander' ) );
-console.log( isParkedHere( 'Jeep Wrangler' ) );
+console.log( isParkedHere( 'black', 'Ford Explorer' ) );
+console.log( isParkedHere( 'blue', 'Nisson GTR' ) );
+console.log( isParkedHere( 'orange', 'Chevy Cruise' ) );
+console.log( isParkedHere( 'green', 'Audi RS 5 Sportback' ) );
+console.log( isParkedHere( 'red', 'Toyota Highlander' ) );
+console.log( isParkedHere( 'purple', 'Jeep Wrangler' ) );
 
 
 
@@ -62,20 +62,20 @@ console.log( isParkedHere( 'Jeep Wrangler' ) );
 //---------------------
 
 
-function takeForASpin( nameOfCar ){
+function takeForASpin( color, nameOfCar ){
   console.log( 'in takeForASpin' );
-  if( isParkedHere( nameOfCar ) ){
+  if( isParkedHere( color, nameOfCar ) ){
     parkedCars.splice( (parkedCars.indexOf( nameOfCar ) ), 1 );
-    console.log( 'Is', nameOfCar, 'ready to be taken out for a spin?' );
+    console.log( 'Is', color, nameOfCar, 'ready to be taken out for a spin?' );
     return true;
   } // end if
-  console.log( 'Is', nameOfCar, 'ready to be taken out for a spin?' );
+  console.log( 'Is', color, nameOfCar, 'ready to be taken out for a spin?' );
   return false;
 } // takeForSpin
 
-console.log( takeForASpin( 'Ford Explorer' ) );
-console.log( takeForASpin( 'Nisson GTR' ) );
-console.log( takeForASpin( 'Chevy Cruise' ) );
-console.log( takeForASpin( 'Audi RS 5 Sportback' ) );
-console.log( takeForASpin( 'Toyota Highlander' ) );
-console.log( takeForASpin( 'Jeep Wrangler' ) );
+console.log( takeForASpin( 'black', 'Ford Explorer' ) );
+console.log( takeForASpin( 'blue', 'Nisson GTR' ) );
+console.log( takeForASpin( 'orange', 'Chevy Cruise' ) );
+console.log( takeForASpin( 'green', 'Audi RS 5 Sportback' ) );
+console.log( takeForASpin( 'red', 'Toyota Highlander' ) );
+console.log( takeForASpin( 'purple', 'Jeep Wrangler' ) );
